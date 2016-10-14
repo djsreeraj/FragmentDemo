53ef245c3db5499b6f0957ad4f4e37b7e6061c27
djsreeraj <sreeraj011m@gmail.com>
2016-10-15 00:34:40 +0530

    Initial commit -fragmnt test -er

---
+++ app/src/main/java/layout/FragmentTwo.java
@@ -0,0 +1,22 @@
+package layout;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.android.fragmentdemo.R;
+
+
+public class FragmentTwo extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment_two, container, false);
+    }
+
+}
